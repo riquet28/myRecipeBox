061ede23330923aeff526354ae0100ee588fb1c5
riquet28 <eric.merlin3@gmail.com>
2017-06-09 04:25:04 +0200

    Creation du header ok

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+
+  convertirPseudo = (pseudo) => {
+      return /[aeiouy]/i.test(pseudo[0]) ? `d'${pseudo}` : `de ${pseudo}`
+  };
+
+  render() {
+    return (
+      <header>
+        <h1>La boite à recette {this.convertirPseudo(this.props.pseudo)}</h1>
+      </header>
+    )
+  }
+
+  static propTypes = {
+    pseudo: React.PropTypes.string.isRequired
+  }
+
+}
+
+export default Header;
