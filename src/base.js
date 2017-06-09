397d70253f2a7c0b1a3698b38cbcdb50e59c2694
riquet28 <eric.merlin3@gmail.com>
2017-06-09 18:22:53 +0200

    Synchroniser les recettes avec firebase ok

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCVVntcEfg8ivjrNj6kX5tytZuLcvb67O4",
+  authDomain: "ma-boite-a-recettes-723f5.firebaseapp.com",
+  databaseURL: "https://ma-boite-a-recettes-723f5.firebaseio.com"
+})
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Admin from './Admin';
 import Card from './Card';
 // Charger les Recettes
 import recettes from '../recettes';
+// Firebase
+import base from '../base';
 
 
 class App extends React.Component {
@@ -12,6 +14,17 @@ class App extends React.Component {
   state = {
     recettes: {}
   };
+
+  componentWillMount() {
+    this.ref = base.syncState( `${this.props.params.pseudo}/recettes`, {
+      context: this,
+      state: 'recettes'
+    } )
+  }
+
+  componentWillUnount() {
+    base.removeBinding(this.ref);
+  }
 
   chargerExemple = () => {
     this.setState({ recettes });
